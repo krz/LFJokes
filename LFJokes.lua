0f2b0202276930b83ac6d90eb43b3cc9c6c3fd98
Christoph <krz@users.noreply.github.com>
2023-04-20 18:54:02 +0200

    first commit

---
+++ LFJokes.lua
@@ -0,0 +1,18 @@
+local LFJokes = CreateFrame("frame", "EventFrame")
+
+-- decode the joke
+local function decode(s)
+   return (s:gsub("[A-Za-z]", function (c)
+       local offset = c:lower() < "n" and 13 or -13
+       return string.char(c:byte() + offset)
+   end))
+end
+
+
+EventUtil.ContinueOnAddOnLoaded("Blizzard_Calendar", function()
+   CalendarViewEventAcceptButton:HookScript("OnClick", function()
+      j = decode(jokes[math.random(#jokes)]))
+      SendChatMessage(j, "SAY") 
+   end
+   )
+end)
